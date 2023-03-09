268c8b6d4d16ddba710cf1cc7d787a79449d4680
Artem Shnurko <shnurko.artyom@gmail.com>
2023-03-09 09:59:54 +0100

    config deploy((

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './components/App.jsx';
-import './index.css';
+// import './index.css';
 
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Hogwarts/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
