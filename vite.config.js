a0bfc401765d702ed372d61d0067d211e9ee114f
Artem <102293717+DarkArtyom@users.noreply.github.com>
2023-03-14 11:49:48 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://darkartyom.github.io/Hogwarts/signin',
+  base: '/Hogwarts/',
   plugins: [react()],
 });
